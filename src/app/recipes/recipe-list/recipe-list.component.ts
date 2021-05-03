da0cbd578115a7b850c48afa32b49db5803dde37
wright1022 <MichaelFWright42@gmail.com>
2021-05-03 00:59:33 -0500

    pass data between recipe components
    event & property binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/10_minute_Recipe_for_a_Healthy_Garden_Salad_-_49859886512.jpg/1200px-10_minute_Recipe_for_a_Healthy_Garden_Salad_-_49859886512.jpg'),
-    new Recipe('Cheese Pizza', 'A lovely cheese pizza, just for me', 'https://kitchenatics.com/wp-content/uploads/2020/09/Cheese-pizza-1.jpg'),
+    new Recipe('Cheese Pizza', 'A lovely cheese pizza, just for me!', 'https://kitchenatics.com/wp-content/uploads/2020/09/Cheese-pizza-1.jpg'),
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
