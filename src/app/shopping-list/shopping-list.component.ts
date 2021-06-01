c763ec3d165616eef17b82b1ec43acb9841248ac
wright1022 <MichaelFWright42@gmail.com>
2021-06-01 17:10:44 -0500

    added template form
    added template driven form to shopping list edit component

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -24,7 +24,12 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
       );
   }
 
+  onEditItem(index: number) {
+    this.slService.startedEditing.next(index);
+  }
+
   ngOnDestroy(): void {
     this.igChangeSub.unsubscribe();
   }
+
 }
