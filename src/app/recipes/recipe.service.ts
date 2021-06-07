927f77c11603ae6947c4cf974fc2a15b77066b8d
wright1022 <MichaelFWright42@gmail.com>
2021-06-07 14:58:01 -0500

    added http service
    changed recipes to add them to http databasse

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,28 +9,33 @@ import { Recipe } from './recipe.model';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Big Kahuna Burger',
-      'This IS a tasty burger!',
-      'https://www.skinnytaste.com/wp-content/uploads/2015/05/grilled-lean-teriyaki-burger-550x390.jpg',
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('Pineapple Slices', 2)
-      ]),
-    new Recipe(
-      'Cheese Pizza',
-      'A lovely cheese pizza, just for me!',
-      'https://kitchenatics.com/wp-content/uploads/2020/09/Cheese-pizza-1.jpg',
-      [
-        new Ingredient('Dough', 1),
-        new Ingredient('Cheese', 1),
-        new Ingredient('Sauce', 1)
-      ]),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Big Kahuna Burger',
+  //     'This IS a tasty burger!',
+  //     'https://www.skinnytaste.com/wp-content/uploads/2015/05/grilled-lean-teriyaki-burger-550x390.jpg',
+  //     [
+  //       new Ingredient('Meat', 1),
+  //       new Ingredient('Pineapple Slices', 2)
+  //     ]),
+  //   new Recipe(
+  //     'Cheese Pizza',
+  //     'A lovely cheese pizza, just for me!',
+  //     'https://kitchenatics.com/wp-content/uploads/2020/09/Cheese-pizza-1.jpg',
+  //     [
+  //       new Ingredient('Dough', 1),
+  //       new Ingredient('Cheese', 1),
+  //       new Ingredient('Sauce', 1)
+  //     ]),
+  // ];
 
-  constructor(private slService: ShoppingListService) {
+  private recipes: Recipe[] = [];
 
+  constructor(private slService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
   }
 
   getRecipes() {
